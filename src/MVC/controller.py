4c402cc86c9b0945d68bce2cf3e272e6acb339fa
Bennett Nguyễn <7akk4r2821@gmail.com>
2023-08-02 00:23:49 +0700

    Implementation of new View component functionalities

--- src/MVC/controller.py
+++ src/MVC/controller.py
@@ -10,6 +10,10 @@ class Controller:
 
     def receive_events(self, events):
         self.model.handle_input(events)
-        
+
     def update_view(self):
-        self.view.draw_tree(self.model.st.root)
+        # collided_node[0]: does mouse collide with queried node
+        # collided_node[1]: return the queried node's ID is mouse does collide with that node
+        collided_node: tuple[bool, int] = self.view.mouse_collide_node(self.model.st.root) 
+        self.view.draw_tree(self.model.st.root, collided_node)
+        self.view.view_array(self.model.st.arr, collided_node)
