cce42d1c7dbf6dd4595cc0ee916d2914a2487f9e
Bennett Nguyễn <7akk4r2821@gmail.com>
2023-08-02 00:25:47 +0700

    Add deprecation note

--- src/preload/system/constants.py
+++ src/preload/system/constants.py
@@ -5,26 +5,26 @@ HEIGHT: int = 700
 HALF_WIDTH: int = WIDTH//2
 HALF_HEIGHT: int = HEIGHT//2
 
-FPS: int = 30
-VERSION: str = "0.1.0-prealpha"
+FPS: int = 48
+VERSION: str = "0.2.0-prealpha"
 
 
 # - RT's algo constants
 # -- Scalar
 SCALE = 200
-VERTICAL_SCALE = 100
+VERTICAL_SCALE = 150
 
-# -- Offset
+# -- Offset (prelim_x offset could be deprecated in the future)
 PRELIM_X_OFFSET: float = 1
 DEPTH_OFFSET: float = 1
 ROOT_DEPTH: float = 0
 
 # -- Shape's properties
-LINE_THICKNESS: int = 3
-NODE_CIRCLE_RADIUS: int = 20
+LINE_THICKNESS: int = 4
+NODE_CIRCLE_RADIUS: int = 40
 
 # -- Distance between nodes and subtrees
-NODE_DISTANCE: float = 0.5
+NODE_DISTANCE: float = 0.7
 SIBLING_DISTANCE: float = 0.0
 TREE_DISTANCE: float = 0.0
 
