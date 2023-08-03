89be279bd50cc534df1256ff06623080ca45e3d8
Bennett Nguyễn <7akk4r2821@gmail.com>
2023-08-03 14:41:21 +0700

    renamed 'collided_node' to 'hovered_node' for clarity

--- src/MVC/controller.py
+++ src/MVC/controller.py
@@ -13,8 +13,9 @@ class Controller:
         self.model.handle_input(events)
 
     def update_view(self):
-        # collided_node[0]: does mouse collide with queried node
-        # collided_node[1]: return the queried node's ID if mouse does collide with that node
-        collided_node: tuple[bool, Node] = self.view.mouse_collide_node(self.model.st.root) 
-        self.view.draw_tree(self.model.st.root, collided_node)
-        self.view.view_array(self.model.st.arr, collided_node)
+        # hovered_node[0]: is the mouse hovering on the queried node
+        # hovered_node[1]: return the queried node's ID if mouse is hovering on that node
+        hovered_node: tuple[bool, Node] = self.view.mouse_hover_node(self.model.st.root) 
+        self.view.draw_tree(self.model.st.root, hovered_node)
+        self.view.view_array(self.model.st.arr, hovered_node)
+        self.view.view_hovered_node_info(hovered_node)
