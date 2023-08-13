67530e81761ee95eb8bfce8682985a000e8db0e8
Bennett Nguyễn <7akk4r2821@gmail.com>
2023-08-13 16:49:46 +0700

    Support App UI inputs, implement new way of loading app's themes, and introduces UI visibility

--- src/MVC/model.py
+++ src/MVC/model.py
@@ -1,9 +1,11 @@
-import os
-import json
+import os, json
 import pygame as pg
+import pygame_gui as pg_gui
 import src.preload.system.constants as const
 import src.preload.algorithm.reingold_tilford as RT
 from src.preload.business_objects.theme import Theme
+from src.preload.business_objects.app_ui import AppUI
+from src.preload.system.app_type import VisibilityField
 from src.preload.business_objects.segment_tree import SegmentTree
 
 from typing import List, Dict
@@ -12,13 +14,24 @@ from typing import List, Dict
 class Model:
     def __init__(self, st: SegmentTree):
         self.st = st
+        self.app_ui = AppUI()
+
+        self.visibility_dict: Dict[VisibilityField, bool] = {
+            const.VIEW_ARRAY_FIELD: True,
+            const.VIEW_NODE_DATA_FIELD: True,
+            const.VIEW_NODE_INFO_FIELD: True,
+            const.DISPLAY_THEME_SELECTION_FIELD: True
+        }
 
         self.current_theme: Theme
         self.themes: Dict[str, Theme] = {}
         self.available_themes: List[str] = []
 
         self.load_themes()
-        self.set_theme("Dark Mode")
+        self.set_theme("Midnight (Built-in)")
+
+        self.app_ui.init_theme_selection_ui(self.available_themes, self.current_theme)
+        self.app_ui.init_visibility_ui(self.visibility_dict)
 
         self.previous_mouse_pos = (0, 0)
         self.current_mouse_pos = (0, 0)
@@ -29,8 +42,29 @@ class Model:
         delta_x = const.HALF_WIDTH - self.st.root.x
         RT.move_node_by_delta_pos(self.st.root, delta_x, 0)
 
-    def handle_input(self, events):
-        self.pan()
+    def handle_input(self, events: List[pg.event.Event]):
+        visibility_ui = self.app_ui.visibility_ui
+        self.change_visibility_dict(visibility_ui.UI.get_multi_selection())
+
+        for event in events:
+            if event.type == pg_gui.UI_DROP_DOWN_MENU_CHANGED:
+                if event.ui_object_id == const.THEME_DROP_DOWN_OBJ_ID:
+                    self.set_theme(event.text)
+                    self.app_ui.set_theme(self.current_theme)
+
+            if event.type == pg.KEYDOWN and event.key == pg.K_q:
+                if visibility_ui.container.visible:
+                    visibility_ui.container.hide()
+                else:
+                    visibility_ui.container.show()
+
+            self.app_ui.GUI_MANAGER.process_events(event)
+
+        if not visibility_ui.container.visible:
+            self.pan()
+
+    def update_ui(self, delta_time: float):
+        self.app_ui.GUI_MANAGER.update(delta_time)
 
     def pan(self):
         mouse_pressed = pg.mouse.get_pressed()
@@ -50,14 +84,26 @@ class Model:
         RT.move_node_by_delta_pos(self.st.root, delta_x, delta_y)
         self.previous_mouse_pos = self.current_mouse_pos
 
+    def change_visibility_dict(self, chosen_field: List[str]):
+        for field, _ in self.visibility_dict.items():
+            self.visibility_dict[field] = field in chosen_field
+
     def load_themes(self):
         for entry in os.listdir("./theme"):
-            if not entry.endswith(".json"):
+            if not entry.endswith(const.THEME_IDENTIFIER_SUFFIX):
                 continue
 
-            with open(f"./theme/{entry}", "r") as f:
+            suffix_start_index = entry.find(const.THEME_IDENTIFIER_SUFFIX)
+            file_name = entry[:suffix_start_index]
+            ui_path = const.DEFAULT_UI_FILE
+
+            with open(f"./theme/{entry}/{file_name}.json", "r") as f:
                 json_obj = json.load(f)
-                self.themes[json_obj["Name"]] = Theme(json_obj["Palette"])
+
+                if not json_obj["use_default_app_ui"]:
+                    ui_path = f"./theme/{entry}/{const.CUSTOM_UI_FILE}"
+
+                self.themes[json_obj["Name"]] = Theme(json_obj, ui_path)
                 self.available_themes.append(json_obj["Name"])
 
     def set_theme(self, name: str):
