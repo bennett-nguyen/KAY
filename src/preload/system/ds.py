961a7a8b7920267b1e68185339c9f3c3b7ebde1a
Bennett Nguyễn <7akk4r2821@gmail.com>
2023-08-13 16:53:53 +0700

    Allow new events for pygame

--- src/preload/system/ds.py
+++ src/preload/system/ds.py
@@ -1,19 +1,26 @@
 import pygame as pg
 import src.preload.system.constants as const
-from pygame.locals import (
-    QUIT,
-    KEYDOWN,
-    KEYUP,
-    MOUSEBUTTONDOWN,
-    MOUSEBUTTONUP
+from pygame.locals import *
+from pygame_gui._constants import (
+    UI_DROP_DOWN_MENU_CHANGED
 )
 
 pg.init()
+pg.event.set_allowed(
+    [
+        QUIT,
+        KEYDOWN,
+        KEYUP,
+        MOUSEBUTTONDOWN,
+        MOUSEBUTTONUP,
+        MOUSEMOTION,
+        UI_DROP_DOWN_MENU_CHANGED
+    ]
+)
 
-screen = pg.display.set_mode((const.WIDTH, const.HEIGHT))
+screen = pg.display.set_mode(const.RESOLUTION)
 clock = pg.time.Clock()
 icon = pg.image.load("./icon/icon.ico").convert_alpha()
 
-pg.event.set_allowed([QUIT, KEYDOWN, KEYUP, MOUSEBUTTONDOWN, MOUSEBUTTONUP])
 pg.display.set_caption(f"Segment Tree Visualizer v{const.VERSION}")
 pg.display.set_icon(icon)
