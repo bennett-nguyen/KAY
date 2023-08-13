b189fe7b8192962d6617cea0d3e7d313a2fbfbe2
Bennett Nguyễn <7akk4r2821@gmail.com>
2023-08-13 16:50:53 +0700

    Add minor changes for the new update of Model and View

--- src/MVC/controller.py
+++ src/MVC/controller.py
@@ -1,8 +1,8 @@
 import pygame as pg
 from src.MVC.view import View
 from src.MVC.model import Model
-from src.preload.business_objects.segment_tree import SegmentTree
 from src.preload.business_objects.node import Node
+from src.preload.business_objects.segment_tree import SegmentTree
 
 from typing import List, Tuple
 
@@ -12,7 +12,8 @@ class Controller:
         self.model = Model(st)
         self.view = View()
 
-    def receive_events(self, events: List[pg.event.Event]):
+    def process(self, events: List[pg.event.Event], delta_time: float):
+        self.model.update_ui(delta_time)
         self.model.handle_input(events)
 
     def update_view(self):
@@ -21,6 +22,9 @@ class Controller:
         hovered_node: Tuple[bool, Node] = self.view.mouse_hover_node(self.model.st.root)
 
         self.view.request_theme(self.model.current_theme)
+        self.view.request_visibility(self.model.visibility_dict)
+
         self.view.draw_tree(self.model.st.root, hovered_node)
         self.view.view_array(self.model.st.arr, hovered_node)
         self.view.view_hovered_node_info(hovered_node)
+        self.view.view_app_ui(self.model.app_ui)
