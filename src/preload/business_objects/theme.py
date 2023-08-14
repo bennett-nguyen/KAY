c582fea503b2c507db0f2e894704d586595b62a4
Bennett Nguyễn <7akk4r2821@gmail.com>
2023-08-14 09:55:16 +0700

    Make 'Theme' a dataclass

--- src/preload/business_objects/theme.py
+++ src/preload/business_objects/theme.py
@@ -1,40 +1,23 @@
 import pygame as pg
-from typing import Dict, Any
-from src.preload.system.app_type import ThemeField, ValidJSONColorFormats
+from dataclasses import dataclass
 
-
+@dataclass(slots=True, frozen=True)
 class Theme:
-    __slots__ = (
-        "NAME",
-        "APP_UI_PATH",
-        "BACKGROUND_CLR",
-        "NODE_FILLINGS_CLR",
-        "NODE_OUTLINE_CLR",
-        "NODE_DISPLAY_DATA_CLR",
-        "LINE_CLR",
-        "NODE_DISPLAY_DATA_HIGHLIGHT_CLR",
-        "NODE_OUTLINE_HIGHLIGHT_CLR"
-    )
+    NAME: str
 
-    def __init__(self, json_obj: Dict[str, Any], app_ui_path: str):
-        name: str = json_obj["Name"]
-        palette_obj: Dict[ThemeField, ValidJSONColorFormats] = json_obj["Palette"]
+    # Background
+    BACKGROUND_CLR: pg.Color
 
-        self.NAME: str = name
+    # Node
+    NODE_FILLINGS_CLR: pg.Color
+    NODE_OUTLINE_CLR: pg.Color
+    NODE_DISPLAY_DATA_CLR: pg.Color
 
-        # Background
-        self.BACKGROUND_CLR: pg.Color = pg.Color(palette_obj["background"])
+    NODE_OUTLINE_HIGHLIGHT_CLR: pg.Color
+    NODE_DISPLAY_DATA_HIGHLIGHT_CLR: pg.Color
 
-        # Node
-        self.NODE_FILLINGS_CLR: pg.Color = pg.Color(palette_obj["node_fillings"])
-        self.NODE_OUTLINE_CLR: pg.Color = pg.Color(palette_obj["node_outline"])
-        self.NODE_DISPLAY_DATA_CLR: pg.Color = pg.Color(palette_obj["node_display_data"])
+    # Line
+    LINE_CLR: pg.Color
 
-        self.NODE_OUTLINE_HIGHLIGHT_CLR: pg.Color = pg.Color(palette_obj["node_outline_highlight"])
-        self.NODE_DISPLAY_DATA_HIGHLIGHT_CLR: pg.Color = pg.Color(palette_obj["node_display_data_highlight"])
-
-        # Line
-        self.LINE_CLR: pg.Color = pg.Color(palette_obj["line"])
-
-        # App UI
-        self.APP_UI_PATH: str = app_ui_path
+    # App UI
+    APP_UI_PATH: str
