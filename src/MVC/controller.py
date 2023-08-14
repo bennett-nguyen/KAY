dcb810f2eaf31056edacbd30db3aba5e35bb9fed
Bennett Nguyễn <7akk4r2821@gmail.com>
2023-08-14 09:57:35 +0700

    Rename abbreviated variables

--- src/MVC/controller.py
+++ src/MVC/controller.py
@@ -8,8 +8,8 @@ from typing import List, Tuple
 
 
 class Controller:
-    def __init__(self, st: SegmentTree):
-        self.model = Model(st)
+    def __init__(self, segment_tree: SegmentTree):
+        self.model = Model(segment_tree)
         self.view = View()
 
     def process(self, events: List[pg.event.Event], delta_time: float):
@@ -19,12 +19,12 @@ class Controller:
     def update_view(self):
         # hovered_node[0]: is the mouse hovering on the queried node
         # hovered_node[1]: return the queried node if mouse is hovering on that node
-        hovered_node: Tuple[bool, Node] = self.view.mouse_hover_node(self.model.st.root)
+        hovered_node: Tuple[bool, Node] = self.view.mouse_hover_node(self.model.segment_tree.root)
 
         self.view.request_theme(self.model.current_theme)
         self.view.request_visibility(self.model.visibility_dict)
 
-        self.view.draw_tree(self.model.st.root, hovered_node)
-        self.view.view_array(self.model.st.arr, hovered_node)
+        self.view.draw_tree(self.model.segment_tree.root, hovered_node)
+        self.view.view_array(self.model.segment_tree.arr, hovered_node)
         self.view.view_hovered_node_info(hovered_node)
         self.view.view_app_ui(self.model.app_ui)
