4bd826b6475ce58a978fc85ebe96bf27c11645c0
Bennett Nguyễn <7akk4r2821@gmail.com>
2023-08-19 12:59:33 +0700

    Prevent View from drawing an empty tree

--- src/MVC/controller.py
+++ src/MVC/controller.py
@@ -1,10 +1,11 @@
+from typing import List, Tuple
+
 import pygame as pg
+
 from src.MVC.view import View
 from src.MVC.model import Model
 from src.preload.business_objects.node import Node
 from src.preload.business_objects.segment_tree import SegmentTree
-
-from typing import List, Tuple
 
 
 class Controller:
@@ -17,14 +18,16 @@ class Controller:
         self.model.handle_input(events)
 
     def update_view(self):
-        # hovered_node[0]: is the mouse hovering on the queried node
-        # hovered_node[1]: return the queried node if mouse is hovering on that node
-        hovered_node: Tuple[bool, Node] = self.view.mouse_hover_node(self.model.segment_tree.root)
-
         self.view.request_theme(self.model.current_theme)
         self.view.request_visibility(self.model.visibility_dict)
+        self.model.app_ui.message_box_ui.request_theme(self.model.current_theme)
 
-        self.view.draw_tree(self.model.segment_tree.root, hovered_node)
-        self.view.view_array(self.model.segment_tree.arr, hovered_node)
-        self.view.view_hovered_node_info(hovered_node)
+        if self.model.segment_tree.array_length != 0:
+            # hovered_node[0]: is the mouse hovering on the queried node
+            # hovered_node[1]: return the queried node if mouse is hovering on that node
+            hovered_node: Tuple[bool, Node] = self.view.mouse_hover_node(self.model.segment_tree.root)
+            self.view.draw_tree(self.model.segment_tree.root, hovered_node)
+            self.view.view_array(self.model.segment_tree.arr, hovered_node)
+            self.view.view_hovered_node_info(hovered_node)
+
         self.view.view_app_ui(self.model.app_ui)
