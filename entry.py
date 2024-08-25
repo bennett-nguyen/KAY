af90477267dcc6366ffb495a2ac9dfdc741fbfd5
Bennett Nguyen <83117848+bennett-nguyen@users.noreply.github.com>
2024-08-25 13:46:06 +0700

    Code revamp

--- entry.py
+++ entry.py
@@ -3,21 +3,21 @@ from sys import exit
 
 import pygame as pg
 
-import src.preload.system.ds as ds
+import src.preload.system.window as window
 import src.preload.system.constants as const
 from src.MVC.controller import Controller
-from src.preload.business_objects.segment_tree import SegmentTree
+from src.preload.tree.segment_tree import SegmentTree
 
-array = [5, 2, 3, 5]
+array = [1, 3, -2, 8, -7]
 invalid_query_val = 0
-query_function = update_function = lambda x, y: x + y
+function = lambda x, y: x + y
 
-st = SegmentTree(array, invalid_query_val, query_function, update_function)
+st = SegmentTree(array, invalid_query_val, function)
 controller = Controller(st)
 
 while True:
-    ds.screen.fill(controller.model.current_theme.BACKGROUND_CLR)
-    dt_time = ds.clock.tick(const.FPS) / 1000.0
+    window.screen.fill(controller.model.current_theme.BACKGROUND_CLR)
+    dt_time = window.clock.tick(const.FPS) / 1000.0
 
     events = pg.event.get()
 
