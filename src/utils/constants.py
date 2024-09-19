e79110f4537ee0b9ce1f0c945c558aec367b4b1a
Bennett Nguyen <83117848+bennett-nguyen@users.noreply.github.com>
2024-09-19 21:46:16 +0700

    Add more constants for drawing lazy values

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -3,9 +3,9 @@ from src.utils import kay_typing
 # - General
 DEFAULT_SCREEN_OCCUPATION_PERCENTAGE: float = 80 / 100
 
-ACTIVE_FPS: int = 48
+ACTIVE_FPS: int = 60
 IDLE_FPS: int = 1
-VERSION: str = "0.8.1-prealpha"
+VERSION: str = "0.9.0-prealpha"
 
 # -- Array viewer and hovered node viewer's constants
 MAX_X_PER_LINE: int = 500
@@ -31,12 +31,12 @@ ROOT_DEPTH: int = 0
 # -- Shape's properties
 CIRCLE_OUTLINE_THICKNESS: int = 4
 LINE_THICKNESS: float = CIRCLE_OUTLINE_THICKNESS / 2.0
-NODE_CIRCLE_RADIUS: int = 45
+NODE_CIRCLE_RADIUS: int = 50
 
 # -- Distance between nodes and subtrees
 NODE_DISTANCE: float = 0.7
 SIBLING_DISTANCE: float = 0.0
-TREE_DISTANCE: float = 0.0
+TREE_DISTANCE: float = 0.1
 
 # -- UI config files
 THEME_IDENTIFIER_SUFFIX: str = "-app-theme"
@@ -46,6 +46,12 @@ CMD_THEME_FILE: str = "theme/cmdline_ui-DO-NOT-EDIT.json"
 COMMAND_BOX_OBJECT_ID = "#command-box"
 COMMAND_BOX_ANCHOR_OFFSET = 20
 COMMAND_BOX_HEIGHT = 50
+
+LAZY_LINE_LENGTH = 20
+LAZY_LINE_THICKNESS = 1
+LAZY_LINE_OFFSET = NODE_CIRCLE_RADIUS - CIRCLE_OUTLINE_THICKNESS - 5
+
+HISTORY_SIZE_LIMIT: int = 100
 
 # Links
 GITHUB_LINK: kay_typing.WebLink = kay_typing.WebLink("https://github.com/bennett-nguyen/KAY")
